65419df40ed57225c1fbe1fdb8187a50682e463d
Fabrizio Guglielmino <guglielmino@gmail.com>
2018-09-29 08:25:32 +0200

    Fix Celery initialization missing files

--- pushetta/pushetta/__init__.py
+++ pushetta/pushetta/__init__.py
@@ -1,2 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+from core import signal
+
 __version__ = '1.1.0'
 __build__ = ''
